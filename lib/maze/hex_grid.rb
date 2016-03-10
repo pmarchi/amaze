6e756e5fbd9eb986b6e0fff3c155aa7cbe610d7d
Patrick Marchi <patrick.marchi@geo.uzh.ch>
2016-03-10 15:11:34 +0100

    + sizeable hexgrids

--- lib/maze/hex_grid.rb
+++ lib/maze/hex_grid.rb
@@ -30,39 +30,77 @@ class Maze::HexGrid < Maze::Grid
     end
   end
   
-  def to_s
+  def to_s marked_cells=[]
+    line = "___" * cell_size
+    space = "   " * cell_size
+    upper_east = "\\"
+    lower_east = "/"
+    upper_west = "/"
+    lower_west = "\\"
+    empty_side = " "
+    
     # output = "\e[H\e[2J" # clear screen
     output = ""
-    
+
     # top row
     repeat = ((columns + 0.5) / 2).round - 1
-    output << " __" << "    __" * repeat << "\n"
+    output << " " * cell_size << line
+    output << (" " * cell_size * 2 + space + line) * repeat << "\n"
     
     each_row do |row|
-      top = "/"
-      bottom = "\\"
+      upper = Array.new(cell_size) { "" }
+      lower = Array.new(cell_size) { "" }
       
-      row.each do |cell|
-        
-        first_row = (cell.row == 0)
-        last_row = (cell.row == rows-1)
-        last_column = (cell.column == columns-1)
-        
-        if cell.column.even?
-          top << "  \\"
-          bottom << "__/"
-        else
-          top_east_boundary = (first_row && last_column) ? "__" : "__/"
-          bottom_east_boundary = (last_row && last_column) ? "" : "  \\"
-          top << top_east_boundary
-          bottom << bottom_east_boundary
-        end
+      cell_size.times do |i|
+        upper[i] << " " * (cell_size-1-i) << upper_west
+        lower[i] << " " * i << lower_west
       end
 
-      output << top << "\n"
-      output << bottom << "\n"
+      row.each do |cell|
+        if cell.column.even?
+          cell_size.times do |i|
+            # upper half of cell
+            north = space + " " * i * 2
+            north_east = (cell.linked? cell.northeast) ? empty_side : upper_east
+            upper[i] << north << north_east
+
+            # lower half of cell
+            south = (i < (cell_size-1) || cell.linked?(cell.south)) ? space + " " * (cell_size-1-i) * 2 : line
+            south_east = (cell.linked? cell.southeast) ? empty_side : lower_east
+            lower[i] << south << south_east
+          end
+        else
+          cell_size.times do |i|
+            # lower half of cell
+            south = (i < (cell_size-1) || cell.linked?(cell.north)) ? space + " " * (cell_size-1-i) * 2 : line
+            south_east = (cell.north || cell.northeast) ? 
+              (cell.north && cell.northeast && cell.north.linked?(cell.northeast)) ? 
+                empty_side : 
+                lower_east :
+              ""
+            upper[i] << south << south_east
+
+            # upper half of cell
+            north = space + " " * i * 2
+            north_east = (cell.linked? cell.northeast) ? empty_side : upper_east
+            lower[i] << north << north_east
+          end
+        end
+      end
+      
+      cell_size.times {|i| output << upper[i] << "\n" }
+      cell_size.times {|i| output << lower[i] << "\n" }
     end
     
+    # last lower half of cells
+    repeat = columns / 2
+    cell_size.times do |i|
+      south = (i < (cell_size-1)) ? space : line
+      output << " " * (cell_size-i)
+      output << (" " * i * 2 + space + lower_west + " " * (cell_size-1-i) * 2 + south + lower_east) * repeat
+      output << "\n"
+    end
+
     output
   end
 end

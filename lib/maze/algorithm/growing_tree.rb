a698ba96c420c75bc6e44c445eb762a129c605bc
Patrick Marchi <patrick.marchi@geo.uzh.ch>
2016-03-10 20:57:58 +0100

    . costmetics

--- lib/maze/algorithm/growing_tree.rb
+++ lib/maze/algorithm/growing_tree.rb
@@ -8,8 +8,10 @@ class Maze::Algorithm::GrowingTree < Maze::Algorithm
       
       # selecting the last element is recursive backtracker
       # choose whatever you want
-      cell = (rand(3) == 0) ? active.sample : active.last
-
+      # cell = (rand(3) == 0) ? active.sample : active.last
+      # cell = active[active.size/2..-1].sample
+      cell = active.last
+      
       neighbor = cell.neighbors.select {|neighbor| neighbor.links.empty? }.sample
       
       if neighbor

015785963accd87d5e5ce34feb4e80a63746510c
Patrick Marchi <patrick.marchi@geo.uzh.ch>
2016-03-10 20:38:02 +0100

    . shorter implementation of growing tree

--- lib/maze/algorithm/growing_tree.rb
+++ lib/maze/algorithm/growing_tree.rb
@@ -1,40 +1,28 @@
 
-
 class Maze::Algorithm::GrowingTree < Maze::Algorithm
   
-  # 1. Let C be a list of cells, initially empty. Add one cell to C, at random.
-  # 2. Choose a cell from C, and carve a passage to any unvisited neighbor of that cell, adding that neighbor to C as well. If there are no unvisited neighbors, remove the cell from C.
-  # 3. Repeat #2 until C is empty.
-  
   def work grid
-    cells = []
-    visited = {}
+    active = [grid.random_cell]
     
-    cell = grid.random_cell
-    cells << cell
-    
-    until cells.empty?
-      cell = cells.last
-      visited[cell] = true
+    while active.any?
+      
+      # selecting the last element is recursive backtracker
+      # choose whatever you want
+      cell = (rand(3) == 0) ? active.sample : active.last
 
-      neighbors = cell.neighbors.dup
+      neighbor = cell.neighbors.select {|neighbor| neighbor.links.empty? }.sample
       
-      candidate = nil
-      until neighbors.empty?
-        candidate = neighbors.delete(neighbors.sample)
-        break unless visited.key? candidate
-        candidate = nil
-      end
-      
-      if candidate
-        cell.link candidate
-        cells << candidate
+      if neighbor
+        cell.link neighbor
+        active << neighbor
       else
-        cells.delete cell
+        active.delete cell
       end
       
-      yield cells
+      yield active
     end
+    
+    grid
   end
   
   def status

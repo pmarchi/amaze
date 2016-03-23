1827c17b7a915965ac9ef546dcbc601c7ed4b892
Patrick Marchi <patrick.marchi@geo.uzh.ch>
2016-03-23 09:38:06 +0100

    . refactor object instance creation into a factory

---
+++ lib/maze/factory.rb
@@ -0,0 +1,23 @@
+
+class Maze::Factory
+  
+  def self.types
+    %i( delta ortho sigma )
+  end
+  
+  # The type of the grid
+  attr_reader :type
+  
+  def initialize type
+    raise "#{type} maze is not supported" unless self.class.types.include? type
+    @type = type
+  end
+  
+  def create_grid *args
+    Maze::Grid.const_get(type.to_s.capitalize).new *args
+  end
+  
+  def create_ascii_formatter *args
+    Maze::Formatter::Ascii.const_get(type.to_s.capitalize).new *args
+  end
+end

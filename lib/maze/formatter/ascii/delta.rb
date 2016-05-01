45160010940f955315db98e767c52058c89d7d92
Patrick Marchi <patrick.marchi@geo.uzh.ch>
2016-05-01 22:53:34 +0200

    + draw_path for delta grid, WIP

--- lib/maze/formatter/ascii/delta.rb
+++ lib/maze/formatter/ascii/delta.rb
@@ -1,36 +1,37 @@
 
 class Maze::Formatter::ASCII::Delta < Maze::Formatter::ASCII
 
-  # FIXME render does not work for masked grids
-
   def draw_cell cell
     x0, y0 = coord cell
     # reverse triangle
     if (cell.column + cell.row).even?
+      unless cell.north
+        # base top row
+        char[y0][x0] = h
+        char[y0][x0+cell_size*2+1] = h
+      end
       1.upto(cell_size*2) do |i|
         # base
-        char[y0][x0+i] = h
-        char[y0][x0] = h unless cell.north
-        char[y0][x0+cell_size*2+1] = h unless cell.north
+        char[y0][x0+i] = h unless cell.linked? cell.north
       end
       0.upto(cell_size) do |i|
         # east
-        char[y0+i+1][x0+cell_size*2+1-i] = re
+        char[y0+i+1][x0+cell_size*2+1-i] = re unless cell.linked? cell.east
         # west
-        char[y0+i+1][x0+i] = rw
+        char[y0+i+1][x0+i] = rw unless cell.linked? cell.west
       end
 
     # normal triangle
     else
       1.upto(cell_size*2) do |i|
         # base
-        char[y0+cell_size+1][x0+i] = h
+        char[y0+cell_size+1][x0+i] = h unless cell.linked? cell.south
       end
       0.upto(cell_size) do |i|
         # east
-        char[y0+i+1][x0+cell_size+1+i] = ne
+        char[y0+i+1][x0+cell_size+1+i] = ne unless cell.linked? cell.east
         # west
-        char[y0+i+1][x0+cell_size-i] = nw
+        char[y0+i+1][x0+cell_size-i] = nw unless cell.linked? cell.west
         
       end
     end
@@ -40,6 +41,10 @@ class Maze::Formatter::ASCII::Delta < Maze::Formatter::ASCII
   end
   
   def draw_path cell
+    x0, y0 = coord cell
+    
+    # FIXME: postition of center differs for odd cells
+    char[y0+cell_size][x0+cell_size] = c.color(content_color_of cell)
   end
   
   def coord cell
@@ -74,6 +79,10 @@ class Maze::Formatter::ASCII::Delta < Maze::Formatter::ASCII
     '/'
   end
   
+  def c
+    '.'
+  end
+  
   alias_method :nw, :re
   alias_method :ne, :rw
   
@@ -93,13 +102,13 @@ __END__
 
 
  ____  ____  ____ 
-\    /\    /\    /
- \  /  \  /  \  /
-  \/____\/____\/
-  /\    /\    /\
- /  \  /  \  /  \
-/____\/____\/____\
-\    /\    /\    /
+\ .   \    /\    /
+ \.... \  /  \  /
+  \  .  \/____\/
+  /  .  /\    /\
+ /.... /. \. /  \
+/ .   /____\/____\
+\ .  /\    /\    /
  \  /  \  /  \  /
   \/____\/____\/
   /\    /\    /\
@@ -110,10 +119,10 @@ __END__
   \/____\/____\/
 
  ______  ______  ______ 
-\      /\      /\      /
- \ *  /  \    /  \    /
-  \  / *  \  /    \  /
-   \/______\/______\/
+\       \      /\      /
+ \ .     \    /  \    /
+  \  ` .  \  /    \  /
+   \ ______\/______\/
    /\      /\      /\
   /  \    /  \    /  \
  /    \  /    \  /    \
@@ -135,9 +144,9 @@ __END__
  __________  __________  __________
 \          /\          /\          /
  \        /  \        /  \        /
-  \  *   /    \      /    \      /
-   \    /  *   \    /      \    /
-    \  /        \  /        \  /
+  \  .        \      /    \      /
+   \    `  .   \    /      \    /
+    \           \  /        \  /
      \/__________\/__________\/
      /\          /\          /\     
     /  \        /  \        /  \    
@@ -157,3 +166,56 @@ __END__
   /      \    /      \    /      \  
  /        \  /        \  /        \ 
 /__________\/__________\/__________\
+
+
+
+
+∙---∙---∙
+ \...\ / \
+  ∙---∙---∙
+ / \ / \ /
+∙---∙---∙
+ \ / \ / \
+  ∙---∙---∙
+
+∙-----∙-----∙
+ \ ....\ * / \
+  \   . \ /   \
+   ∙  .  ∙-----∙
+  /.... / \   /
+ / .   /   \ /
+∙  .  ∙-----∙
+ \ . / \   / \
+  \ /   \ /   \
+   ∙-----∙-----∙
+
+∙-------∙-------∙
+ \     / \     / \
+  \ ..... \   /   \
+   \ /     \ /     \
+    ∙-------∙-------∙
+   / \     / \     / 
+  /   \   /   \   /
+ /     \ /     \ /
+∙-------∙-------∙
+ \     / \     / \
+  \   /   \   /   \
+   \ /     \ /     \
+    ∙-------∙-------∙
+
+∙---------∙---------∙
+ \       / \       / \
+  \  .      \     /   \
+   \    ` .  \   /     \
+    \ /       \ /       \
+     ∙---------∙---------∙
+    / \       / \       / 
+   /   \     /   \     / 
+  /     \   /     \   /
+ /       \ /       \ /
+∙---------∙---------∙
+ \       / \       / \
+  \     /   \     /   \
+   \   /     \   /     \
+    \ /       \ /       \
+     ∙---------∙---------∙

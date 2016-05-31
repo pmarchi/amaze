57bd320ddc6527b7cce67443ca62d260059e027c
Patrick Marchi <mail@patrickmarchi.ch>
2016-05-31 18:40:57 +0200

    . update read_char

--- lib/amaze/script.rb
+++ lib/amaze/script.rb
@@ -206,18 +206,16 @@ class Amaze::Script
   
   # Reads keypresses from the user including 2 and 3 escape character sequences.
   def read_char
-    begin
-      STDIN.echo = false
-      STDIN.raw!
-      input = STDIN.getc.chr
-      if input == "\e" then
-        input << STDIN.read_nonblock(3) rescue nil
-        input << STDIN.read_nonblock(2) rescue nil
-      end
-    ensure
-      STDIN.echo = true
-      STDIN.cooked!
+    STDIN.echo = false
+    STDIN.raw!
+    input = STDIN.getc.chr
+    if input == "\e" then
+      input << STDIN.read_nonblock(3) rescue nil
+      input << STDIN.read_nonblock(2) rescue nil
     end
     input
+  ensure
+    STDIN.echo = true
+    STDIN.cooked!
   end
 end
